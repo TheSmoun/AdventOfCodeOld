4d1e903d223b416a8a7522e7ad4f27b34b235ef9
Simon Grossmann <simon.grossmann93@gmail.com>
2018-12-01 18:29:05 +0100

    Code improvements.

--- 2017/3/puzzle2.py
+++ 2017/3/puzzle2.py
@@ -1,7 +1,5 @@
 import math
-
-number = 289326
-numbers = {(0, 0): 1}
+from itertools import product
 
 def get_next(x, y):
     if x == 0 and y == 0: return (1, 0)
@@ -10,10 +8,14 @@ def get_next(x, y):
     if y <= -x and x < y: return (x, y - 1)
     if y <= -x and x >= y: return (x + 1, y)
 
+number = 289326
+numbers = {(0, 0): 1}
+o = [-1, 0, 1]
 x = 0
 y = 0
+
 while numbers[(x, y)] <= number:
     x, y = get_next(x, y)
-    numbers[(x, y)] = sum(numbers.get((x + i, y + j), 0) for i in [-1, 0, 1] for j in [0, 1, -1])
+    numbers[(x, y)] = sum(numbers.get((x + t[0], y + t[1]), 0) for t in product(o, o))
 
 print(numbers[(x, y)])
