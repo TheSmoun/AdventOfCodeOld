8651cad7a2be019514932538b922deff9fb117c9
Simon Grossmann <simon.grossmann93@gmail.com>
2018-12-02 11:29:01 +0100

    Shortened code.

--- 2018/2/puzzle1.py
+++ 2018/2/puzzle1.py
@@ -4,8 +4,7 @@ with open("2018/2/input.txt", "r") as f:
     lines = [line.strip() for line in f.readlines()]
     count2 = 0
     count3 = 0
-    for line in lines:
-        counts = Counter(line).items()
+    for counts in map(lambda l: Counter(l).items(), lines):
         if len([count for char, count in counts if count == 2]) > 0:
             count2 += 1
         if len([count for char, count in counts if count == 3]) > 0:
