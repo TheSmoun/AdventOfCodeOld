94571a411aaabb7b131fc6b5b99e51569dd9d6a7
Simon Grossmann <simon.grossmann93@gmail.com>
2018-12-02 11:26:49 +0100

    Shortened code.

--- 2018/2/puzzle1.py
+++ 2018/2/puzzle1.py
@@ -5,14 +5,10 @@ with open("2018/2/input.txt", "r") as f:
     count2 = 0
     count3 = 0
     for line in lines:
-        has2 = False
-        has3 = False
-        for char, count in Counter(line).items():
-            if count == 2 and not has2:
-                count2 += 1
-                has2 = True
-            if count == 3 and not has3:
-                count3 += 1
-                has3 = True
+        counts = Counter(line).items()
+        if len([count for char, count in counts if count == 2]) > 0:
+            count2 += 1
+        if len([count for char, count in counts if count == 3]) > 0:
+            count3 += 1
     
     print(count2 * count3)
