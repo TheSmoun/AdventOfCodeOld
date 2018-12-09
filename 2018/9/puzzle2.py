6a5db67a419d6128f5567e374d442a8abd520683
Simon Grossmann <simon.grossmann93@gmail.com>
2018-12-09 11:56:33 +0100

    Solved 2018/9.

---
+++ 2018/9/puzzle2.py
@@ -0,0 +1,18 @@
+from collections import deque, defaultdict
+
+players = 431
+marbles = 7095000
+
+game = deque([0])
+scores = defaultdict(int)
+
+for m in range(1, marbles + 1):
+    if m % 23 == 0:
+        game.rotate(7)
+        scores[m % players] += m + game.pop()
+        game.rotate(-1)
+    else:
+        game.rotate(-1)
+        game.append(m)
+
+print(max(scores.values()))
