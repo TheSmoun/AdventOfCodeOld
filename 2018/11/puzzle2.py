aefe0ad11c542b66b4286b49fe84341ef21d68aa
Simon Grossmann <simon.grossmann93@gmail.com>
2018-12-12 00:12:36 +0100

    Shortened code.

--- 2018/11/puzzle2.py
+++ 2018/11/puzzle2.py
@@ -4,19 +4,13 @@ serial_number = 5177
 grid = defaultdict(int)
 for x in range(1, 301):
     for y in range(1, 301):
-        p = ((((x + 10) * y + serial_number) * (x + 10) // 100) % 10) - 5
-        grid[(x, y)] = p
-
-for x in range(1, 301):
-    for y in range(1, 301):
-        if x == 1 and y == 1:
-            grid[(1, 1)] = grid[(1, 1)]
-        elif y == 1:
-            grid[(x, 1)] = grid[(x - 1, 1)] + grid[(x, 1)]
-        elif x == 1:
-            grid[(1, y)] = grid[(1, y - 1)] + grid[(1, y)]
-        else:
-            grid[(x, y)] = grid[(x, y)] + grid[(x - 1, y)] + grid[(x, y - 1)] - grid[(x - 1, y - 1)]
+        grid[(x, y)] = ((((x + 10) * y + serial_number) * (x + 10) // 100) % 10) - 5
+        if x > 1:
+            grid[(x, y)] += grid[(x - 1, y)]
+        if y > 1:
+            grid[(x, y)] += grid[(x, y - 1)]
+        if x > 1 and y > 1:
+            grid[(x, y)] -= grid[(x - 1, y - 1)]
 
 s = 0
 x_s = None
