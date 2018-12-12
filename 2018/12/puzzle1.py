9287c7a4d377b697c220c5aeaccaea79bf602991
Simon Grossmann <simon.grossmann93@gmail.com>
2018-12-12 11:37:17 +0100

    Shorter rules parsing.

--- 2018/12/puzzle1.py
+++ 2018/12/puzzle1.py
@@ -4,11 +4,8 @@ with open("2018/12/input.txt", "r") as f:
     lines = [line.strip() for line in f.readlines() if len(line.strip()) > 0]
     state = lines[0].split(" ")[-1]
     length = len(state)
+    rules = {i:o for (i, o) in map(lambda l: l.split(" => "), lines[1:])}
 
-    rules = {}
-    for (i, o) in map(lambda l: l.split(" => "), lines[1:]):
-        rules[i] = o
-    
     for _ in range(20):
         if not state.startswith("....") or not state.endswith("...."):
             state = "...." + state + "...."
