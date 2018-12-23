c1338278793839a0e9e895f80a676690c715adaa
Simon Grossmann <simon.grossmann93@gmail.com>
2018-12-23 22:26:18 +0100

    Solved 2018/23 part 1.

---
+++ 2018/23/puzzle1.py
@@ -0,0 +1,13 @@
+import re
+
+def dist(a, b):
+    return sum(abs(a[i] - b[i]) for i in range(3))
+
+with open("2018/23/input.txt", "r") as f:
+    lines = [list(map(int, re.findall(r'-?\d+', line.strip()))) for line in f.readlines()]
+    range_2_pos = {r: (x, y, z) for x, y, z, r in lines}
+    positions = [(x, y, z) for x, y, z, _ in lines]
+
+    max_range = max(range_2_pos.keys())
+    max_range_pos = range_2_pos[max_range]
+    print(len([pos for pos in positions if dist(max_range_pos, pos) <= max_range]))
