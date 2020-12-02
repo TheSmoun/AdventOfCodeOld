25fdcdd720f56ce8a4a40b66f9d277eddb53e81e
Simon Grossmann <simon.grossmann93@gmail.com>
2020-12-02 11:07:19 +0100

    Using regex instead of split.

--- 2020/src/main/kotlin/de/thesmoun/aoc/days/Day2.kt
+++ 2020/src/main/kotlin/de/thesmoun/aoc/days/Day2.kt
@@ -3,9 +3,8 @@ package de.thesmoun.aoc.days
 class Day2 : Day<Collection<Pair<Day2.PasswordPolicy, String>>, Int>("Day 2: Password Philosophy") {
 
     override fun parseInput(input: Collection<String>): Collection<Pair<PasswordPolicy, String>> = input.map {
-        val parts = it.split(' ', ':')
-        val rangeParts = parts[0].split('-')
-        Pair(PasswordPolicy(rangeParts[0].toInt(), rangeParts[1].toInt(), parts[1][0]), parts[3])
+        val (min, max, char, password) = Regex("(\\d+)-(\\d+) (.): (\\w+)").find(it)!!.destructured
+        Pair(PasswordPolicy(min.toInt(), max.toInt(), char[0]), password)
     }
 
     override fun runPart1(input: Collection<Pair<PasswordPolicy, String>>) = input.count {
