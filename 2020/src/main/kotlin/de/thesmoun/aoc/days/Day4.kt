f47665490e81373c097c8dc40074a7f003f33f0c
Simon Grossmann <simon.grossmann93@gmail.com>
2020-12-04 22:04:17 +0100

    Refactoring.

--- 2020/src/main/kotlin/de/thesmoun/aoc/days/Day4.kt
+++ 2020/src/main/kotlin/de/thesmoun/aoc/days/Day4.kt
@@ -3,15 +3,21 @@ package de.thesmoun.aoc.days
 import java.util.*
 import kotlin.collections.HashMap
 
-class Day4 : Day<Collection<Map<String, Day4.PassportField>>, Int>("Day 4: Passport Processing") {
+class Day4 : Day<Collection<Map<String, String>>, Int>("Day 4: Passport Processing") {
 
-    companion object {
-        private val requiredFields = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
-    }
+    private val requiredFields = mapOf(
+            Pair("byr", Regex("19[2-9][0-9]|200[0-2]")),
+            Pair("iyr", Regex("201[0-9]|2020")),
+            Pair("eyr", Regex("202[0-9]|2030")),
+            Pair("hgt", Regex("1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in")),
+            Pair("hcl", Regex("#[0-9a-f]{6}")),
+            Pair("ecl", Regex("amb|blu|brn|gry|grn|hzl|oth")),
+            Pair("pid", Regex("[0-9]{9}"))
+    )
 
-    override fun parseInput(input: Collection<String>): Collection<Map<String, PassportField>> {
-        val passports = LinkedList<Map<String, PassportField>>()
-        var passport = HashMap<String, PassportField>()
+    override fun parseInput(input: Collection<String>): Collection<Map<String, String>> {
+        val passports = LinkedList<Map<String, String>>()
+        var passport = HashMap<String, String>()
         passports.add(passport)
 
         input.forEach {
@@ -21,16 +27,7 @@ class Day4 : Day<Collection<Map<String, Day4.PassportField>>, Int>("Day 4: Passp
             } else {
                 it.split(' ').forEach { pair ->
                     val split = pair.split(':')
-                    when (val key = split[0]) {
-                        "byr" -> passport[key] = ByrField(split[1])
-                        "iyr" -> passport[key] = IyrField(split[1])
-                        "eyr" -> passport[key] = EyrField(split[1])
-                        "hgt" -> passport[key] = HgtField(split[1])
-                        "hcl" -> passport[key] = HclField(split[1])
-                        "ecl" -> passport[key] = EclField(split[1])
-                        "pid" -> passport[key] = PidField(split[1])
-                        "cid" -> passport[key] = CidField(split[1])
-                    }
+                    passport[split[0]] = split[1]
                 }
             }
         }
@@ -38,60 +35,11 @@ class Day4 : Day<Collection<Map<String, Day4.PassportField>>, Int>("Day 4: Passp
         return passports
     }
 
-    override fun runPart1(input: Collection<Map<String, PassportField>>) = input.count {
-        requiredFields.all { f -> it.containsKey(f) }
+    override fun runPart1(input: Collection<Map<String, String>>) = input.count {
+        requiredFields.keys.all { f -> it.containsKey(f) }
     }
 
-    override fun runPart2(input: Collection<Map<String, PassportField>>) = input.count {
-        requiredFields.all { f -> it.containsKey(f) } && it.values.all { f -> f.isValid() }
-    }
-
-    abstract class PassportField(protected val value: String) {
-        abstract fun isValid(): Boolean
-    }
-
-    abstract class YearField(value: String, private val range: IntRange) : PassportField(value) {
-        override fun isValid(): Boolean {
-            val intValue = value.toIntOrNull() ?: return false
-            return intValue in range
-        }
-    }
-
-    abstract class RegexField(value: String, private val regex: Regex) : PassportField(value) {
-        override fun isValid() = regex.matches(value)
-    }
-
-    class ByrField(value: String) : YearField(value, 1920..2002)
-
-    class IyrField(value: String) : YearField(value, 2010..2020)
-
-    class EyrField(value: String) : YearField(value, 2020..2030)
-
-    class HgtField(value: String) : PassportField(value) {
-        override fun isValid(): Boolean {
-            when {
-                value.endsWith("cm") -> {
-                    val intValue = value.substringBefore("cm").toIntOrNull() ?: return false
-                    return intValue in 150..193
-                }
-                value.endsWith("in") -> {
-                    val intValue = value.substringBefore("in").toIntOrNull() ?: return false
-                    return intValue in 59..76
-                }
-                else -> {
-                    return false
-                }
-            }
-        }
-    }
-
-    class HclField(value: String) : RegexField(value, Regex("#[0-9a-f]{6}"))
-
-    class EclField(value: String) : RegexField(value, Regex("(amb|blu|brn|gry|grn|hzl|oth)"))
-
-    class PidField(value: String) : RegexField(value, Regex("[0-9]{9}"))
-
-    class CidField(value: String) : PassportField(value) {
-        override fun isValid() = true
+    override fun runPart2(input: Collection<Map<String, String>>) = input.count {
+        requiredFields.all { f -> it.containsKey(f.key) && f.value.matches(it[f.key] ?: error("")) }
     }
 }
