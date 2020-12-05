91d49fb3b0ae2f9ad90e25863dfb6d1d879336ad
Simon Grossmann <simon.grossmann93@gmail.com>
2020-12-05 10:27:49 +0100

    Refactoring.

--- 2020/src/main/kotlin/de/thesmoun/aoc/days/Day5.kt
+++ 2020/src/main/kotlin/de/thesmoun/aoc/days/Day5.kt
@@ -4,16 +4,9 @@ class Day5 : Day<Collection<Int>, Int>("Day 5: Binary Boarding") {
 
     override fun parseInput(input: Collection<String>) = input.map {
         it.replace(Regex("[FL]"), "0").replace(Regex("[BR]"), "1").toInt(2)
-    }
+    }.sorted()
 
-    override fun runPart1(input: Collection<Int>) = input.maxOrNull() ?: error("")
+    override fun runPart1(input: Collection<Int>) = input.last()
 
-    override fun runPart2(input: Collection<Int>): Int {
-        val sortedIds = input.sorted()
-        for (i in sortedIds.first()..sortedIds.last()) {
-            if (!sortedIds.contains(i))
-                return i
-        }
-        error("")
-    }
+    override fun runPart2(input: Collection<Int>) = (input.first()..input.last()).first { it !in input }
 }
