36efb5ceb98af361abe5fcb1be9c0ebf454ef5ea
Simon Grossmann <simon.grossmann93@gmail.com>
2020-12-06 10:28:47 +0100

    Refactoring.

--- 2020/src/main/kotlin/de/thesmoun/aoc/days/Day6.kt
+++ 2020/src/main/kotlin/de/thesmoun/aoc/days/Day6.kt
@@ -6,11 +6,11 @@ class Day6 : Day<Collection<Collection<String>>, Int>("Day 6: Custom Customs") {
 
     override fun parseInput(input: Collection<String>) = input.splitAt("")
 
-    override fun runPart1(input: Collection<Collection<String>>) = input.map {
+    override fun runPart1(input: Collection<Collection<String>>) = input.sumBy {
         it.flatMap { s -> s.toList() }.toSet().size
-    }.sum()
+    }
 
-    override fun runPart2(input: Collection<Collection<String>>) = input.map {
+    override fun runPart2(input: Collection<Collection<String>>) = input.sumBy {
         it.map { s -> s.toSet() }.reduce { acc, set -> acc intersect set }.size
-    }.sum()
+    }
 }
