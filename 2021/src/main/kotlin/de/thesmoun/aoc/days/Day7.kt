c2d4154981c6da9f53a903eec1ec4ac6e7d1d6a2
Simon Grossmann <simon.grossmann@promatis.de>
2021-12-07 09:04:02 +0100

    Refactoring.

--- 2021/src/main/kotlin/de/thesmoun/aoc/days/Day7.kt
+++ 2021/src/main/kotlin/de/thesmoun/aoc/days/Day7.kt
@@ -8,29 +8,9 @@ class Day7 : Day<Map<Int, Int>, Int>("Day 7: The Treachery of Whales") {
     override fun parseInput(input: Collection<String>) = input.first().split(',').map { it.toInt() }
         .groupingBy { it }.eachCount()
 
-    override fun runPart1(input: Map<Int, Int>): Int {
-        val min = input.keys.minOrNull()!!
-        val max = input.keys.maxOrNull()!!
+    override fun runPart1(input: Map<Int, Int>) = (input.keys.minOrNull()!!..input.keys.maxOrNull()!!)
+        .minOf { i -> input.map { abs(it.key - i) * it.value }.sum() }
 
-        var minFuel = Int.MAX_VALUE
-        (min..max).forEach { i ->
-            val fuel = input.map { abs(it.key - i) * it.value }.sum()
-            minFuel = min(minFuel, fuel)
-        }
-
-        return minFuel
-    }
-
-    override fun runPart2(input: Map<Int, Int>): Int {
-        val min = input.keys.minOrNull()!!
-        val max = input.keys.maxOrNull()!!
-
-        var minFuel = Int.MAX_VALUE
-        (min..max).forEach { i ->
-            val fuel = input.map { (0..abs(it.key - i)).sum() * it.value }.sum()
-            minFuel = min(minFuel, fuel)
-        }
-
-        return minFuel
-    }
+    override fun runPart2(input: Map<Int, Int>) = (input.keys.minOrNull()!!..input.keys.maxOrNull()!!)
+        .minOf { i -> input.map { (0..abs(it.key - i)).sum() * it.value }.sum() }
 }
