f66411a3dfcbfe97e7f0ac315770dc3bcbc64a30
Simon Grossmann <simon.grossmann@promatis.de>
2021-12-07 14:08:25 +0100

    Refactoring.

--- 2021/src/main/kotlin/de/thesmoun/aoc/days/Day7.kt
+++ 2021/src/main/kotlin/de/thesmoun/aoc/days/Day7.kt
@@ -7,9 +7,10 @@ class Day7 : Day<Map<Int, Int>, Int>("Day 7: The Treachery of Whales") {
     override fun parseInput(input: Collection<String>) = input.first().split(',').map { it.toInt() }
         .groupingBy { it }.eachCount()
 
-    override fun runPart1(input: Map<Int, Int>) = (input.keys.minOrNull()!!..input.keys.maxOrNull()!!)
-        .minOf { i -> input.map { abs(it.key - i) * it.value }.sum() }
+    override fun runPart1(input: Map<Int, Int>) = run(input) { it }
 
-    override fun runPart2(input: Map<Int, Int>) = (input.keys.minOrNull()!!..input.keys.maxOrNull()!!)
-        .minOf { i -> input.map { (0..abs(it.key - i)).sum() * it.value }.sum() }
+    override fun runPart2(input: Map<Int, Int>) = run(input) { it * (it + 1) / 2 }
+
+    private fun run(input: Map<Int, Int>, cost: (Int) -> Int) = (input.keys.minOrNull()!!..input.keys.maxOrNull()!!)
+        .minOf { i -> input.map { cost(abs(it.key - i)) * it.value }.sum() }
 }
