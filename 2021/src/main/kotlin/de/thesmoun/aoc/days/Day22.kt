efd9d348c5255f17130d1011090a1ed9ff2661cc
Simon Grossmann <simon.grossmann93@gmail.com>
2021-12-30 10:39:36 +0100

    Minor refactoring for part 2.

--- 2021/src/main/kotlin/de/thesmoun/aoc/days/Day22.kt
+++ 2021/src/main/kotlin/de/thesmoun/aoc/days/Day22.kt
@@ -1,6 +1,6 @@
 package de.thesmoun.aoc.days
 
-class Day22 : Day<Collection<Day22.Instruction>, Int>("Day 22: Reactor Reboot") {
+class Day22 : Day<Collection<Day22.Instruction>, Long>("Day 22: Reactor Reboot") {
 
     private val regex = Regex("^(on|off) x=([-]?\\d+)\\.\\.([-]?\\d+),y=([-]?\\d+)\\.\\.([-]?\\d+),z=([-]?\\d+)\\.\\.([-]?\\d+)$")
 
@@ -9,7 +9,7 @@ class Day22 : Day<Collection<Day22.Instruction>, Int>("Day 22: Reactor Reboot") 
         Instruction(flag == "on", x1.toInt()..x2.toInt(), y1.toInt()..y2.toInt(), z1.toInt()..z2.toInt())
     }
 
-    override fun runPart1(input: Collection<Instruction>): Int {
+    override fun runPart1(input: Collection<Instruction>): Long {
         val points = (0..100).map { (0..100).map { (0..100).map { false }.toBooleanArray() } }
         val range = -50..50
         input.forEach {
@@ -24,14 +24,19 @@ class Day22 : Day<Collection<Day22.Instruction>, Int>("Day 22: Reactor Reboot") 
             }
         }
 
-        return points.sumOf { p -> p.sumOf { it.count { f -> f } } }
+        return points.sumOf { p -> p.sumOf { it.count { f -> f } } }.toLong()
     }
 
-    override fun runPart2(input: Collection<Instruction>): Int {
+    override fun runPart2(input: Collection<Instruction>): Long {
         TODO("Not yet implemented")
     }
 
-    class Instruction(val flag: Boolean, val x: IntRange, val y: IntRange, val z: IntRange)
+    class Instruction(val flag: Boolean, val x: IntRange, val y: IntRange, val z: IntRange) {
+        operator fun contains(point: Triple<Int, Int, Int>): Boolean {
+            val (px, py, pz) = point
+            return px in x && py in y && pz in z
+        }
+    }
 
     private fun IntRange.coerce(range: IntRange) = when {
         range.first > last || range.last < first -> IntRange.EMPTY
