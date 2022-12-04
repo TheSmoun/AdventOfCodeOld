8a1096bc1b6245d4400be4eaa6ff6b2ed741d9a6
Simon Grossmann <simon.grossmann93@gmail.com>
2022-12-04 20:41:20 +0100

    2022 day 4

---
+++ 2022_C#/Advent of Code 2022/Days/Day04.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+using Advent_of_Code_2022.Extensions;
+
+namespace Advent_of_Code_2022.Days;
+
+public class Day04 : DayBase<IEnumerable<(Range, Range)>, int>
+{
+    private const string Pattern = @"^(\d+)\-(\d+),(\d+)\-(\d+)$";
+    private static readonly Regex Regex = new(Pattern);
+    
+    public override string Name => "Day 4: Camp Cleanup";
+
+    public override IEnumerable<(Range, Range)> ParseInput(IEnumerable<string> lines)
+        => lines.Select(l =>
+        {
+            var match = Regex.Match(l);
+            return (new Range(int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value) + 1),
+                new Range(int.Parse(match.Groups[3].Value), int.Parse(match.Groups[4].Value) + 1));
+        });
+
+    public override int RunPart1(IEnumerable<(Range, Range)> input)
+        => input.Count(i => i.Item1.Contains(i.Item2) || i.Item2.Contains(i.Item1));
+
+    public override int RunPart2(IEnumerable<(Range, Range)> input)
+        => input.Count(i => i.Item1.Overlaps(i.Item2) || i.Item2.Overlaps(i.Item1));
+}
---
+++ 2022_C#/Advent of Code 2022/Extensions/RangeEx.cs
@@ -0,0 +1,15 @@
+﻿namespace Advent_of_Code_2022.Extensions;
+
+public static class RangeEx
+{
+    public static bool Contains(this Range r1, Range r2)
+    {
+        return r1.Start.Value <= r2.Start.Value && r1.End.Value >= r2.End.Value;
+    }
+
+    public static bool Overlaps(this Range r1, Range r2)
+    {
+        return r1.Start.Value <= r2.Start.Value && r1.End.Value > r2.Start.Value 
+               || r1.End.Value >= r2.End.Value && r1.Start.Value < r2.End.Value;
+    }
+}
