f533c2e27aa29b029ef794f4b66373ccd51d0727
Simon Grossmann <simon.grossmann93@gmail.com>
2022-12-04 20:17:49 +0100

    2022 day 3 part 2.

--- 2022_C#/Advent of Code 2022/Days/Day03.cs
+++ 2022_C#/Advent of Code 2022/Days/Day03.cs
@@ -2,25 +2,26 @@
 
 namespace Advent_of_Code_2022.Days;
 
-public class Day03 : DayBase<IEnumerable<(string, string)>, int>
+public class Day03 : DayBase<IEnumerable<string>, int>
 {
     public override string Name => "Day 3: Rucksack Reorganization";
 
-    public override IEnumerable<(string, string)> ParseInput(IEnumerable<string> lines)
-        => lines.Select(l => (l[..(l.Length / 2)], l[(l.Length / 2)..]));
+    public override IEnumerable<string> ParseInput(IEnumerable<string> lines) => lines;
 
-    public override int RunPart1(IEnumerable<(string, string)> input)
-        => input.Select(r => r.Item1.Intersect(r.Item2).First())
-            .Sum(i => i switch
-            {
-                >= 'A' and <= 'Z' => i - 'A' + 27,
-                >= 'a' and <= 'z' => i - 'a' + 1,
-                _ => throw new UnreachableException()
-            });
+    public override int RunPart1(IEnumerable<string> input)
+        => input.Select(l => (l[..(l.Length / 2)], l[(l.Length / 2)..]))
+            .Select(r => r.Item1.Intersect(r.Item2).First())
+            .Sum(GetItemPriority);
 
-    public override int RunPart2(IEnumerable<(string, string)> input)
+    public override int RunPart2(IEnumerable<string> input)
+        => input.Chunk(3)
+            .Select(g => g[0].Intersect(g[1]).Intersect(g[2]).First())
+            .Sum(GetItemPriority);
+
+    private static int GetItemPriority(char item) => item switch
     {
-        // throw new NotImplementedException();
-        return default;
-    }
+        >= 'A' and <= 'Z' => item - 'A' + 27,
+        >= 'a' and <= 'z' => item - 'a' + 1,
+        _ => throw new UnreachableException()
+    };
 }
