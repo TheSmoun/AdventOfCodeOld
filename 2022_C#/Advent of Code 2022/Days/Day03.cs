0ca0b7a9b87b1e2e49a36a521330661d66e9bd10
Simon Grossmann <simon.grossmann93@gmail.com>
2022-12-04 20:12:45 +0100

    2022 day 3 part 1.

---
+++ 2022_C#/Advent of Code 2022/Days/Day03.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace Advent_of_Code_2022.Days;
+
+public class Day03 : DayBase<IEnumerable<(string, string)>, int>
+{
+    public override string Name => "Day 3: Rucksack Reorganization";
+
+    public override IEnumerable<(string, string)> ParseInput(IEnumerable<string> lines)
+        => lines.Select(l => (l[..(l.Length / 2)], l[(l.Length / 2)..]));
+
+    public override int RunPart1(IEnumerable<(string, string)> input)
+        => input.Select(r => r.Item1.Intersect(r.Item2).First())
+            .Sum(i => i switch
+            {
+                >= 'A' and <= 'Z' => i - 'A' + 27,
+                >= 'a' and <= 'z' => i - 'a' + 1,
+                _ => throw new UnreachableException()
+            });
+
+    public override int RunPart2(IEnumerable<(string, string)> input)
+    {
+        // throw new NotImplementedException();
+        return default;
+    }
+}
