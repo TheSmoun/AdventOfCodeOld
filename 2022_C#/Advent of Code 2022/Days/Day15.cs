28e2766ba7a66bbab1dbcd929e076df4a2d5359c
Simon Grossmann <simon.grossmann@appgineering.com>
2022-12-15 20:09:50 +0100

    2022 day 15.

--- 2022_C#/Advent of Code 2022/Days/Day15.cs
+++ 2022_C#/Advent of Code 2022/Days/Day15.cs
@@ -1,9 +1,10 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
 using Advent_of_Code_2022.Lib;
 
 namespace Advent_of_Code_2022.Days;
 
-public partial class Day15 : DayBase<List<Day15.Sensor>, int>
+public partial class Day15 : DayBase<List<Day15.Sensor>, BigInteger>
 {
     private static readonly Regex Regex = ParsingRegex();
     
@@ -22,22 +23,76 @@ public partial class Day15 : DayBase<List<Day15.Sensor>, int>
         }).ToList();
     }
 
-    public override int RunPart1(List<Sensor> input)
+    public override BigInteger RunPart1(List<Sensor> input)
     {
-        var coveredRanges = input.Select(s => s.GetCoveredRange(10))
-            .Where(r => r.HasValue)
-            .Select(r => r!.Value)
-            .ToArray();
+        const int y = 10;
+
+        var ranges = GetRanges(input, y);
+        var min = ranges.Min(r => r.Start);
+        var max = ranges.Max(r => r.End);
+
+        var count = 0;
+        for (var x = min; x <= max; x++)
+        {
+            if (ranges.Any(r => r.Contains(x)) && !input.Any(i => i.Beacon.Y == y && i.Beacon.X == x))
+                count++;
+        }
+
+        return count;
+    }
+
+    public override BigInteger RunPart2(List<Sensor> input)
+    {
+        const int searchSpace = 4000000;
+
+        var result = new BigInteger();
+        for (var y = 0; y <= searchSpace; y++)
+        {
+            var ranges = GetRanges(input, y,
+                r => new Range<int>(Math.Max(0, r.Start), Math.Min(searchSpace, r.End)));
+
+            var merged = true;
+            var firstRange = ranges[0];
+            while (merged && ranges.Count > 1)
+            {
+                merged = false;
+
+                var secondRange = ranges[1];
+                if (firstRange.Start <= secondRange.Start && firstRange.End >= secondRange.Start)
+                {
+                    firstRange.End = Math.Max(firstRange.End, secondRange.End);
+                    ranges.RemoveAt(1);
+                    merged = true;
+                }
+            }
+
+            if (!merged || firstRange.Start != 0 || firstRange.End != searchSpace)
+            {
+                result = (BigInteger)(firstRange.End + 1) * searchSpace + y;
+                break;
+            }
+        }
         
-        throw new NotImplementedException();
+        return result;
     }
 
-    public override int RunPart2(List<Sensor> input)
+    private static List<Range<int>> GetRanges(IEnumerable<Sensor> sensors, int y,
+        Func<Range<int>, Range<int>>? filter = null)
     {
-        throw new NotImplementedException();
+        var query = sensors.Select(s => s.GetCoveredRange(y))
+            .Where(r => r.HasValue)
+            .Select(r => r!.Value);
+
+        if (filter is not null)
+        {
+            query = query.Order()
+                .Select(filter);
+        }
+
+        return query.ToList();
     }
 
-    public struct Sensor
+    public readonly struct Sensor
     {
         public Vec2<int> Position { get; }
         public Vec2<int> Beacon { get; }
@@ -51,14 +106,14 @@ public partial class Day15 : DayBase<List<Day15.Sensor>, int>
             Distance = Math.Abs(diff.X) + Math.Abs(diff.Y);
         }
 
-        public IntRange? GetCoveredRange(int y)
+        public Range<int>? GetCoveredRange(int y)
         {
             var verticalDistance = Math.Abs(y - Position.Y);
             if (verticalDistance > Distance)
                 return null;
 
             var margin = Distance - verticalDistance;
-            return new IntRange(Position.X - margin, Position.X + margin);
+            return new Range<int>(Position.X - margin, Position.X + margin);
         }
     }
 
---
+++ 2022_C#/Advent of Code 2022/Lib/Range.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+namespace Advent_of_Code_2022.Lib;
+
+[DebuggerDisplay("{Start} -> {End}")]
+public struct Range<TNumber> : IComparable<Range<TNumber>>
+    where TNumber : INumber<TNumber>
+{
+    public TNumber Start { get; set; }
+    public TNumber End { get; set; }
+    
+    public Range(TNumber start, TNumber end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public bool Contains(TNumber i)
+    {
+        return i >= Start && i <= End;
+    }
+
+    public int CompareTo(Range<TNumber> other)
+    {
+        var startComparison = Start.CompareTo(other.Start);
+        if (startComparison != 0)
+            return startComparison;
+        return End.CompareTo(other.End);
+    }
+}
